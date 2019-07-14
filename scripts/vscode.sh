29268ceca50ff4fbf4badf5082df4ba34a4f72b6
Maxime Dréau <maxime@dreau.fr>
2019-07-14 16:20:36 +0200

    Add VS Code package installation

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/microsoft.gpg
+sudo install -o root -g root -m 644 /tmp/microsoft.gpg /etc/apt/trusted.gpg.d/
+rm /tmp/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y code
+
+
 # List extensions with `code --list-extensions`
 
 i() {
