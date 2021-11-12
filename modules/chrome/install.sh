3804d381c40d273452dacca48f7c046e527c6995
Maxime Dréau <maxime@dreau.fr>
2021-11-12 19:30:53 +0100

    chrome: disable hardware media key
    so that controls go to Spotify

--- modules/chrome/install.sh
+++ modules/chrome/install.sh
@@ -10,6 +10,26 @@ xdg-settings set default-web-browser google-chrome.desktop
 mkdir -p "$HOME"/.config/google-chrome/
 touch "$HOME"/.config/google-chrome/First\ Run
 
+if [[ ! -f "$HOME"/.config/google-chrome/Local\ State ]]; then
+  # Start and stop chrome to initialize $HOME/.config/google-chrome/Local State
+  google-chrome &
+  sleep 1
+  kill %%
+  sleep 1 # wait a bit for chrome to fully save its file
+fi
+
+# Disable Hardware Media Key Handling so that Play/Pause goes to Spotify
+if ! grep -q 'hardware-media-key-handling@2' "$HOME"/.config/google-chrome/Local\ State; then
+  echo 'Disabling hardware media key'
+  if grep -q 'enabled_labs_experiments' "$HOME"/.config/google-chrome/Local\ State; then
+    # append
+    sed -i 's/enabled_labs_experiments":\[/enabled_labs_experiments":["hardware-media-key-handling@2",/' "$HOME"/.config/google-chrome/Local\ State
+  else
+    # create
+    sed --debug -i 's/"browser":{/"browser":{"enabled_labs_experiments":["hardware-media-key-handling@2"],/' "$HOME"/.config/google-chrome/Local\ State
+  fi
+fi
+
 echo "
 You can now start Chrome:
 - login to your Google account to fetch extensions
